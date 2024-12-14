7c6fa067195dc362a9c2fd2f398450fbd63afe8f
Jonathan Kammerland <jkammerland@gmail.com>
2024-12-14 10:35:41 +0100

    Upgraded CLI interface

--- py-gen/src/binding_generator.cpp
+++ py-gen/src/binding_generator.cpp
@@ -1,6 +1,7 @@
 #include "py-gen.h"
 
 #include <cxxopts.hpp>
+#include <exception>
 #include <fmt/format.h>
 #include <llvm/Support/CommandLine.h>
 #include <llvm/Support/raw_ostream.h>
@@ -8,40 +9,115 @@
 #include <string>
 #include <vector>
 
+struct ProgramOptions {
+    std::string              moduleName;
+    size_t                   nSourceFiles;
+    std::string              outputDir = ".";
+    std::string              configFile;
+    std::vector<std::string> clangArgs;
+};
+
+/**
+ * @brief Parses command line arguments to configure the Python binding generator.
+ *
+ * This function uses the cxxopts library to parse command line arguments and populate
+ * a ProgramOptions structure with the provided values. It supports the following options:
+ *
+ * - `-m, --module <module>`: Specifies the name of the Python module to generate bindings for.
+ * - `-s, --sources <files>`: Specifies the source files to generate bindings from.
+ * - `-o, --output <directory>`: Specifies the output directory for the generated bindings (default is current directory).
+ * - `-c, --config <file>`: Specifies a configuration file (currently unsupported parsing).
+ * - `-h, --help`: Prints the usage information and exits.
+ *
+ * If required options are missing or if there is an error in parsing, the function will print
+ * an error message and exit the program.
+ *
+ * @param argc The number of command line arguments.
+ * @param argv The array of command line arguments.
+ * @return A ProgramOptions structure populated with the parsed values.
+ *
+ * Example usage:
+ * @code
+ * ./py-gen --module MyModule --sources file1.cpp file2.cpp --output ./bindings -- -std=c++11 -I/usr/include
+ * @endcode
+ */
+bool parse2Options(int argc, const char **argv, ProgramOptions &programOptions) {
+    cxxopts::Options options("py-gen", "Python binding generator for C++");
+
+    options.add_options()("m,module", "Python module name", cxxopts::value<std::string>());
+    options.add_options()("s,sources", "Source files", cxxopts::value<std::vector<std::string>>());
+    options.add_options()("o,output", "Output directory", cxxopts::value<std::string>()->default_value("."));
+    options.add_options()("c,config", "Config file", cxxopts::value<std::string>());
+
+    // Allow unmatched arguments to be passed through to clang
+    options.allow_unrecognised_options();
+
+    try {
+        auto result = options.parse(argc, argv);
+
+        if (result.count("help")) {
+            llvm::outs() << options.help() << "\n";
+            exit(0);
+        }
+
+        if (!result.count("module")) {
+            llvm::errs() << "Missing module name\n";
+            exit(1);
+        }
+
+        if (!result.count("sources")) {
+            llvm::errs() << "Missing source files\n";
+            exit(1);
+        }
+
+        if (result.count("config")) {
+            programOptions.configFile = result["config"].as<std::string>();
+            llvm::outs() << "Config file: " << programOptions.configFile << " unsupported parsing\n";
+        }
+
+        programOptions.moduleName   = result["module"].as<std::string>();
+        programOptions.clangArgs    = result["sources"].as<std::vector<std::string>>();
+        programOptions.nSourceFiles = programOptions.clangArgs.size();
+        programOptions.outputDir    = result["output"].as<std::string>();
+
+        // List all unmatched arguments
+        if (!result.unmatched().empty()) {
+            llvm::outs() << "Taking as clang tool arguments (+ source provided files, but not listed here):\n";
+            programOptions.clangArgs.reserve(programOptions.clangArgs.size() + result.unmatched().size());
+
+            // Add back "--" separator if we have compiler args, this is lost in cxxopts otherwise
+            programOptions.clangArgs.emplace_back("--");
+        }
+        for (auto &arg : result.unmatched()) {
+            llvm::outs() << "   " << arg << "\n";
+            programOptions.clangArgs.emplace_back(std::move(arg));
+        }
+        return true;
+    } catch (const std::exception &e) {
+        llvm::errs() << "Error parsing options: " << e.what() << "\n";
+        return false;
+    }
+}
+
 int main(int argc, const char **argv) {
-    if (argc < 2) {
-        llvm::errs() << "Usage: " << argv[0] << "<module-name> <source-files>... [compiler-args...]\n";
+    ProgramOptions options;
+    if (not parse2Options(argc, argv, options)) {
         return 1;
     }
 
-    // Print all args
-    for (int i = 0; i < argc; ++i) {
-        llvm::outs() << "argv[" << i << "] = " << argv[i] << "\n";
+    // Prepare clang tool
+    std::vector<const char *> clangArgv;
+    clangArgv.push_back(argv[0]); // Program name
+    for (const auto &arg : options.clangArgs) {
+        clangArgv.push_back(arg.c_str());
     }
 
-    // Copy argv except for argv[1] (module name), but include argv[0] (program name)
-    int          newArgc = argc - 1;
-    const char **newArgv = new const char *[newArgc];
-    newArgv[0]           = argv[0];
-    for (int i = 2; i < argc; ++i) {
-        newArgv[i - 1] = argv[i];
-    }
+    int  argc_          = clangArgv.size();
+    auto expectedParser = clang::tooling::CommonOptionsParser::create(argc_, clangArgv.data(), llvm::cl::getGeneralCategory());
 
-    // print new args
-    for (int i = 0; i < newArgc; ++i) {
-        llvm::outs() << "newArgv[" << i << "] = " << newArgv[i] << "\n";
-    }
-
-    std::string moduleName = argv[1];
-
-    auto expectedParser = clang::tooling::CommonOptionsParser::create(newArgc, newArgv, llvm::cl::getGeneralCategory());
     if (!expectedParser) {
         llvm::errs() << "Error parsing command line arguments: " << expectedParser.takeError() << "\n";
         return 1;
-    }
-
-    for (const auto &sourcePath : expectedParser->getSourcePathList()) {
-        llvm::outs() << "Source path: " << sourcePath << "\n";
     }
 
     clang::tooling::ClangTool tool(expectedParser->getCompilations(), expectedParser->getSourcePathList());
@@ -91,7 +167,7 @@ int main(int argc, const char **argv) {
         }
     }
 
-    generateBindings(structs, functions, headers, moduleName);
+    generateBindings(structs, functions, headers, options.moduleName);
 
     return 0;
 }
